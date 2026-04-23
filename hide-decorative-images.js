230418fbff99ec103542e27a531fe7c1fcb3814c
Sharon C. <99379001+schris48@users.noreply.github.com>
2026-04-23 14:43:26 -0400

    Update hide-decorative-images.js

--- hide-decorative-images.js
+++ hide-decorative-images.js
@@ -1,17 +1,20 @@
-videojs.getPlayer('FaSDPMPnM').ready(function () {
+videojs.registerPlugin('hideDecorativeImages', function () {
   var player = this;
 
-  // Poster image
-  var poster = player.el().querySelector('.vjs-poster');
-  if (poster) {
-    poster.setAttribute('aria-hidden', 'true');
-    poster.setAttribute('role', 'presentation');
-  }
+  player.ready(function () {
 
-  // Thumbnail previews
-  var thumbnails = player.el().querySelectorAll('.vjs-thumbnail-display');
-  thumbnails.forEach(function (thumb) {
-    thumb.setAttribute('aria-hidden', 'true');
-    thumb.setAttribute('role', 'presentation');
+    // Poster image
+    var poster = player.el().querySelector('.vjs-poster');
+    if (poster) {
+      poster.setAttribute('aria-hidden', 'true');
+      poster.setAttribute('role', 'presentation');
+    }
+
+    // Thumbnail previews (may appear later)
+    var thumbnails = player.el().querySelectorAll('.vjs-thumbnail-display');
+    thumbnails.forEach(function (thumb) {
+      thumb.setAttribute('aria-hidden', 'true');
+      thumb.setAttribute('role', 'presentation');
+    });
   });
 });
